96430e862e9f5c68a8e82b046b033d297b3af4d8
Matias Bazan <mati.bazan97@gmail.com>
2024-11-28 20:34:25 -0300

    Nuevo logo | Login

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,28 +1,57 @@
-import { useState } from 'react';
-import './Login.css';
+import { useState } from "react";
+import "./Login.css";
 
 function Login({ onLogin }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (username === 'admin' && password === '1234') {
-      onLogin();
-    } else {
-      alert('Usuario o contraseña incorrectos');
+    setErrorMessage("");
+
+    try {
+      const response = await fetch(
+        "https://c22-26-ft-webapp.onrender.com/auth/login/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: username, password }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem("authToken", data.token);
+        onLogin();
+      } else {
+        setErrorMessage("Usuario o contraseña incorrectos");
+      }
+    } catch (error) {
+      setErrorMessage("Error al conectar con el servidor");
     }
   };
 
   return (
     <div className="login-container">
       <form className="login-form" onSubmit={handleSubmit}>
+        <div className="logo-container">
+          <img
+            src="src/assets/TimePlanify_Logo.png"
+            alt="TimePlanify Logo"
+            className="login-logo"
+          />
+        </div>
         <h2>Inicia Sesión</h2>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
         <div>
-          <label htmlFor="username">Usuario:</label>
+          <label htmlFor="username">Correo Electrónico:</label>
           <input
-            type="text"
+            type="email"
             id="username"
+            placeholder="Ingresa tu correo"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
@@ -33,6 +62,7 @@ function Login({ onLogin }) {
           <input
             type="password"
             id="password"
+            placeholder="Ingresa tu contraseña"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
