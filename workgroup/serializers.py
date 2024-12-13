7b4b80d877facdf26d0ac6ac43eecf5b549afff8
Felipe <39388875+lfazocar@users.noreply.github.com>
2024-12-13 16:18:54 -0300

    Astro Frontend (#32)
    * feat: replace frontend with astro
    
    * feat: add sign up, login and logout pages
    
    * feat: add workgroups index page
    
    * chore: add frontend .env.dist
    
    * feat: add tasks to workgroup view
    
    ---------
    
    Co-authored-by: Felipe <lfazocar@users.noreply.github.com>

--- workgroup/serializers.py
+++ workgroup/serializers.py
@@ -2,12 +2,21 @@ from rest_framework import serializers
 from .models import Workgroup, Task
 
 
+class MemberListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.email
+
+
 class WorkgroupSerializer(serializers.ModelSerializer):
+    members = MemberListingField(many=True, read_only=True)
+    tasks = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Workgroup
-        fields = ['name', 'description']
+        fields = ["name", "description", "members", "tasks"]
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'timestamp', 'workgroup']
+        fields = ["name", "description", "timestamp", "workgroup"]
